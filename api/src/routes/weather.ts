acc232b0ea8add899bb7c43211024821afe6df67
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-09 00:42:25 +0200

    feat: adds check for authentication token inside route for weather

--- api/src/routes/weather.ts
+++ api/src/routes/weather.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import { getWeatherData } from "../controllers/weatherController";
+import { requireAuth } from "../middleware/requireAuth";
 
 export const router = express.Router();
+
+// Check for authentication (token) = require auth for all cities routes
+router.use(requireAuth);
 
 // GET weather for specific city
 router.get("/:location", getWeatherData);
