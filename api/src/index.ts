ce6e2e1bf295e6364b96a999064475b1dd97b562
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-08 18:38:52 +0200

    feat: adds creating of app using express library, middleware, connection to the DB and creates the weather route

---
+++ api/src/index.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import { router as weatherRoutes } from "./routes/weather";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+// APP creating
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes
+app.use("/api/weather", weatherRoutes);
+
+// Connection to DB
+mongoose
+  .connect(process.env.MONGO_URI || "")
+  .then(() => {
+    // Listen for request
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `[api]: Connected to DB & Server is running at http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
