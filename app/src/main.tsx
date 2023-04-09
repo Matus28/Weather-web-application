06a4e10752291e60299ff8004d77129755c805df
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-09 18:18:58 +0200

    feat: configures providers for App, adds context provider for authorization, queryClient provider, SnackBar provider and import ReactQuery-devtools only in dev enviroment

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { SnackBarContextProvider } from "./context/SnackbarContext";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthContextProvider } from "./context/AuthContext";
+
+const queryClient: QueryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <SnackBarContextProvider>
           <App />
+        </SnackBarContextProvider>
+        {import.meta.env.DEV && <ReactQueryDevtools />}
+      </QueryClientProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
