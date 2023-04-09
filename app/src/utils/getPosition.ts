59deb01c8373b453b061ec6f93d7f09c472fe970
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-09 15:34:11 +0200

    feat: creates function for getting geolocation of the user/client

---
+++ app/src/utils/getPosition.ts
@@ -0,0 +1,23 @@
+import { AlertColor } from "@mui/material";
+
+const getPosition = (
+  options?: PositionOptions
+): Promise<GeolocationPosition> => {
+  return new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(resolve, reject, options)
+  );
+};
+
+export const getLocation = async (
+  showSnackBar: (text: string, typeColor: AlertColor) => void
+): Promise<GeolocationPosition | undefined> => {
+  try {
+    const position = await getPosition();
+    return position;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error.message);
+      showSnackBar("Position could not be found!", "error");
+    }
+  }
+};
