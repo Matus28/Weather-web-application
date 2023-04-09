7e72637cd3adc1afbe35453adbe1efa69be4134c
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-09 09:27:16 +0200

    feat: adds custom hook for managing states about user, users data at Signup and Login

---
+++ app/src/hooks/useSignup.ts
@@ -0,0 +1,48 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { UserActionType } from "../context/AuthContext";
+import { useAuthContext } from "./useAuthContext";
+
+export interface FetchUser {
+  email: string;
+  token: string;
+  error?: Error;
+}
+
+export interface FetchData {
+  email: string;
+  password: string;
+}
+
+export const useSignup = () => {
+  const { dispatch } = useAuthContext();
+  return useMutation({
+    mutationFn: async (value: FetchData) => {
+      try {
+        const { data } = await axios.post("/api/user/signup", value, {
+          validateStatus(status) {
+            return status === 200;
+          },
+        });
+        return data as FetchUser;
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          throw new Error(error.response?.data.error);
+        }
+        if (error instanceof Error) {
+          throw new Error("Something went wrong");
+        }
+      }
+    },
+    onError: (error: Error) => {
+      console.log(error);
+    },
+    onSuccess: (data) => {
+      if (data) {
+        localStorage.setItem("user", JSON.stringify(data));
+        dispatch({ type: UserActionType.LOGIN, payload: data });
+        console.log("Success !!!");
+      }
+    },
+  });
+};
