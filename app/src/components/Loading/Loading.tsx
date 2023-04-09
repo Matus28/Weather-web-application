8dabf77dfe61fafda5d09b355604032bf1e7121d
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-09 18:21:43 +0200

    feat: creates Loading component that is shown while data is not yet avalible

---
+++ app/src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import "./Loading.css";
+
+const Loading = (): JSX.Element => {
+  return (
+    <section className="sec-loading">
+      <div className="one"></div>
+    </section>
+  );
+};
+
+export default Loading;
