44ac269f5bc0173a7b263fbc31fb3ff4a33da133
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-12 16:10:59 +0200

    refactor: adds config for dealing wiht image import problems

--- app/jest.config.js
+++ app/jest.config.js
@@ -13,6 +13,8 @@ export default {
     ],
   },
   moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/utils/imgMock.ts",
     "^.+\\.(css|scss)$": "<rootDir>/src/utils/CSSStub.ts",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
