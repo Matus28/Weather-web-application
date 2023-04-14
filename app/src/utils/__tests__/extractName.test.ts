41d808a8e4c2c096ac0bede54f5df6fef0925101
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-15 01:44:42 +0200

    test: adds test for testing function for extracting city name from the City object

--- app/src/utils/__tests__/extractName.test.ts
+++ app/src/utils/__tests__/extractName.test.ts
@@ -1,29 +1,35 @@
 import { City } from "../types";
 import { extractName } from "../extractName";
 
-// export const extractName = (data?: City[]): string[] => {
-//   const result = data && data.map((city: City) => city.cityName);
+const cityList: City[] = [
+  {
+    _id: "001",
+    cityName: "Bratislava",
+    userValue: "1",
+    isDefault: false,
+  },
+  {
+    _id: "002",
+    cityName: "Kosice",
+    userValue: "1",
+    isDefault: false,
+  },
+];
 
-//   return result ?? [];
-// };
-
-describe("extracName from the City object", () => {
-  const cityList: City[] = [
-    {
-      _id: "001",
-      cityName: "Bratislava",
-      userValue: "1",
-      isDefault: false,
-    },
-    {
-      _id: "002",
-      cityName: "Kosice",
-      userValue: "1",
-      isDefault: false,
-    },
-  ];
-
-  test("returns stings with the length 2", () => {
+describe("extracName - function for extracting city name from the City object", () => {
+  test("returns string array with the length 2", () => {
     expect(extractName(cityList).length).toBe(2);
+  });
+
+  test("return string of the first city Bratislava", () => {
+    expect(extractName(cityList)[0]).toBe("Bratislava");
+  });
+
+  test("return string of the second city Kosice", () => {
+    expect(extractName(cityList)[1]).toBe("Kosice");
+  });
+
+  test("returns empty array if props are not given", () => {
+    expect(extractName().length).toBe(0);
   });
 });
