77600b3337d29dde7ec450cfd3b9e5fedfb19158
Matus Tatarko <matus.tatarko28@gmail.com>
2023-04-18 11:16:59 +0200

    refactor: change the name of mock data

--- app/src/utils/__tests__/filterConditionData.test.ts
+++ app/src/utils/__tests__/filterConditionData.test.ts
@@ -1,134 +1,134 @@
 import { filterConditionData } from "../filterConditionData";
-import { testWeatherData } from "../../mockdata/data";
+import { testBAWeatherData } from "../../mockdata/data";
 
 describe("filterConditionData test", () => {
   test("returns array of airConditionData with length 5", () => {
-    const filteredData = filterConditionData(testWeatherData);
+    const filteredData = filterConditionData(testBAWeatherData);
     expect(filteredData.length).toBe(5);
   });
 
   test("returns condition with tile Wind speed", () => {
-    const filteredData = filterConditionData(testWeatherData)[0];
+    const filteredData = filterConditionData(testBAWeatherData)[0];
     expect(filteredData.condition).toBe("Wind speed");
   });
 
   test("returns variable of air condition ", () => {
-    const filteredData = filterConditionData(testWeatherData)[0];
+    const filteredData = filterConditionData(testBAWeatherData)[0];
     expect(filteredData.variable).toBe("wind_kph");
   });
 
   test("returns value of wind speed", () => {
-    const filteredData = filterConditionData(testWeatherData)[0];
+    const filteredData = filterConditionData(testBAWeatherData)[0];
     expect(filteredData.value).toBe(3.6);
   });
 
   test("returns unit of wind speed", () => {
-    const filteredData = filterConditionData(testWeatherData)[0];
+    const filteredData = filterConditionData(testBAWeatherData)[0];
     expect(filteredData.unit).toBe("km/h");
   });
 
   test("returns object that have image icon representing condition", () => {
-    const filteredData = filterConditionData(testWeatherData)[0];
+    const filteredData = filterConditionData(testBAWeatherData)[0];
     expect(filteredData.hasOwnProperty("iconURL")).toBeTruthy();
   });
 
   test("returns condition with tile Wind direction", () => {
-    const filteredData = filterConditionData(testWeatherData)[1];
+    const filteredData = filterConditionData(testBAWeatherData)[1];
     expect(filteredData.condition).toBe("Wind direction");
   });
 
   test("returns variable of air condition ", () => {
-    const filteredData = filterConditionData(testWeatherData)[1];
+    const filteredData = filterConditionData(testBAWeatherData)[1];
     expect(filteredData.variable).toBe("wind_dir");
   });
 
   test("returns value of wind direction", () => {
-    const filteredData = filterConditionData(testWeatherData)[1];
+    const filteredData = filterConditionData(testBAWeatherData)[1];
     expect(filteredData.value).toBe("WSW");
   });
 
   test("returns unit of wind direction (none)", () => {
-    const filteredData = filterConditionData(testWeatherData)[1];
+    const filteredData = filterConditionData(testBAWeatherData)[1];
     expect(filteredData.unit).toBe("");
   });
 
   test("returns object that have image icon representing condition", () => {
-    const filteredData = filterConditionData(testWeatherData)[1];
+    const filteredData = filterConditionData(testBAWeatherData)[1];
     expect(filteredData.hasOwnProperty("iconURL")).toBeTruthy();
   });
 
   test("returns condition with tile Real feel", () => {
-    const filteredData = filterConditionData(testWeatherData)[2];
+    const filteredData = filterConditionData(testBAWeatherData)[2];
     expect(filteredData.condition).toBe("Real feel");
   });
 
   test("returns variable of air condition ", () => {
-    const filteredData = filterConditionData(testWeatherData)[2];
+    const filteredData = filterConditionData(testBAWeatherData)[2];
     expect(filteredData.variable).toBe("feelslike_c");
   });
 
   test("returns value of real feel temperature", () => {
-    const filteredData = filterConditionData(testWeatherData)[2];
+    const filteredData = filterConditionData(testBAWeatherData)[2];
     expect(filteredData.value).toBe(13.2);
   });
 
   test("returns unit of real feel temperature", () => {
-    const filteredData = filterConditionData(testWeatherData)[2];
+    const filteredData = filterConditionData(testBAWeatherData)[2];
     expect(filteredData.unit).toBe("°C");
   });
 
   test("returns object that have image icon representing condition", () => {
-    const filteredData = filterConditionData(testWeatherData)[2];
+    const filteredData = filterConditionData(testBAWeatherData)[2];
     expect(filteredData.hasOwnProperty("iconURL")).toBeTruthy();
   });
 
   test("returns condition with tile UV Index", () => {
-    const filteredData = filterConditionData(testWeatherData)[3];
+    const filteredData = filterConditionData(testBAWeatherData)[3];
     expect(filteredData.condition).toBe("UV Index");
   });
 
   test("returns variable of air condition ", () => {
-    const filteredData = filterConditionData(testWeatherData)[3];
+    const filteredData = filterConditionData(testBAWeatherData)[3];
     expect(filteredData.variable).toBe("uv");
   });
 
   test("returns value of UV Index", () => {
-    const filteredData = filterConditionData(testWeatherData)[3];
+    const filteredData = filterConditionData(testBAWeatherData)[3];
     expect(filteredData.value).toBe(5);
   });
 
   test("returns unit of UV Index, (none)", () => {
-    const filteredData = filterConditionData(testWeatherData)[3];
+    const filteredData = filterConditionData(testBAWeatherData)[3];
     expect(filteredData.unit).toBe("");
   });
 
   test("returns object that have image icon representing condition", () => {
-    const filteredData = filterConditionData(testWeatherData)[3];
+    const filteredData = filterConditionData(testBAWeatherData)[3];
     expect(filteredData.hasOwnProperty("iconURL")).toBeTruthy();
   });
 
   test("returns condition with tile Humidity", () => {
-    const filteredData = filterConditionData(testWeatherData)[4];
+    const filteredData = filterConditionData(testBAWeatherData)[4];
     expect(filteredData.condition).toBe("Humidity");
   });
 
   test("returns variable of air condition ", () => {
-    const filteredData = filterConditionData(testWeatherData)[4];
+    const filteredData = filterConditionData(testBAWeatherData)[4];
     expect(filteredData.variable).toBe("humidity");
   });
 });
 
 test("returns value of humidity", () => {
-  const filteredData = filterConditionData(testWeatherData)[4];
+  const filteredData = filterConditionData(testBAWeatherData)[4];
   expect(filteredData.value).toBe(33);
 });
 
 test("returns unit of humidity", () => {
-  const filteredData = filterConditionData(testWeatherData)[4];
+  const filteredData = filterConditionData(testBAWeatherData)[4];
   expect(filteredData.unit).toBe("%");
 });
 
 test("returns object that have image icon representing condition", () => {
-  const filteredData = filterConditionData(testWeatherData)[4];
+  const filteredData = filterConditionData(testBAWeatherData)[4];
   expect(filteredData.hasOwnProperty("iconURL")).toBeTruthy();
 });
