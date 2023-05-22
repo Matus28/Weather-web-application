983152eeb1dc11702e289f533663599e5c3e06e3
Matus Tatarko <matus.tatarko28@gmail.com>
2023-05-22 14:36:54 +0200

    test: refactor function to return empty string if email is incorrect and adds unit test file for function

--- app/src/utils/getUsername.ts
+++ app/src/utils/getUsername.ts
@@ -1,4 +1,7 @@
 export const getUsername = (email: string): string => {
   const breakIndex = email.indexOf("@");
+
+  if (breakIndex < 0) return "";
+
   return email.split("").slice(0, breakIndex).join("");
 };
