46dadc681181b7ae8c66aafa3b137073c7f12b5d
Matus Tatarko <matus.tatarko28@gmail.com>
2023-05-22 02:32:41 +0200

    feat: adds Drawer Menu for MenuAppBar component

---
+++ app/src/components/DrawerMenu/DrawerMenu.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link, Navigate } from "react-router-dom";
+
+interface Page {
+  text: string;
+  path: string;
+}
+
+const pages: Page[] = [
+  {
+    text: "Home",
+    path: "/",
+  },
+  {
+    text: "Cities",
+    path: "/cities",
+  },
+];
+
+export default function DrawerMenu() {
+  const [state, setState] = React.useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState(open);
+    };
+  const list = () => (
+    <Box
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {pages.map((page: Page, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton onClick={() => setState(false)}>
+              <ListItemIcon>
+                <FiberManualRecordIcon style={{ color: "#FFFFFF" }} />
+              </ListItemIcon>
+              <Link to={page.path}>{page.text}</Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}>
+        <MenuIcon style={{ color: "#FFFFFF" }} />
+      </Button>
+      <Drawer
+        anchor={"left"}
+        open={state}
+        onClose={toggleDrawer(false)}
+        PaperProps={{
+          sx: {
+            background: "#0d252e",
+            width: "220px",
+            color: "#FFFFFF",
+            height: "200px",
+            top: "60px",
+          },
+        }}
+      >
+        {list()}
+      </Drawer>
+    </div>
+  );
+}
